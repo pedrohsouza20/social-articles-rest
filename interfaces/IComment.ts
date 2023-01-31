e0a3a788403bd9f7e6476d6de840547f84c3defe
Pedro <contato.pedromoc@gmail.com>
2023-01-31 12:45:53 -0300

    Criada interface de Comment

---
+++ interfaces/IComment.ts
@@ -0,0 +1,9 @@
+export default interface IComment {
+    id?: Number,
+    body?: String,
+    authorId?: Number,
+    articleId?: Number,
+    edited?: Number,
+    likes?: Number,
+    unlikes?: Number
+}
