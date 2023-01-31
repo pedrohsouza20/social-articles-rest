7d463bbf2dd7e9f266b2cb83e492d1ff4b4a0e28
Pedro <contato.pedromoc@gmail.com>
2023-01-31 12:44:48 -0300

    Criada interface de Article

---
+++ interfaces/IArticle.ts
@@ -0,0 +1,10 @@
+export default interface IArticle {
+    id?: Number,
+    title?: String,
+    body?: String,
+    authorId?: Number,
+    categoryId?: Number,
+    edited?: Number,
+    likes?: Number,
+    unlikes?: Number
+}
